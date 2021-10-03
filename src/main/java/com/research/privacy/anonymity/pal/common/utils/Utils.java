27d775bfb5ca11783544f06b72a62df2b58aca86
Nick Kapetanas <nkapetanas@outlook.com>
2021-10-04 01:21:06 +0300

    AnonymityPal-added Utils file

--- src/main/java/com/research/privacy/anonymity/pal/common/utils/Utils.java
+++ src/main/java/com/research/privacy/anonymity/pal/common/utils/Utils.java
@@ -1,4 +1,51 @@
 package com.research.privacy.anonymity.pal.common.utils;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+@Slf4j
 public class Utils {
+
+    private Utils() {
+    }
+
+    public static boolean isEmpty(Object value) {
+        if (value == null) {
+            return true;
+        } else if (value instanceof String) {
+            return ((String) value).trim().length() == 0;
+        } else if (value instanceof Collection) {
+            return ((Collection) value).isEmpty();
+        } else if (value instanceof Map) {
+            return ((Map) value).isEmpty();
+        } else {
+            return value.getClass().isArray() && Array.getLength(value) == 0;
+        }
+    }
+
+
+    public static boolean isNotEmpty(Object value) {
+        return !isEmpty(value);
+    }
+
+    public static <T> List<T> safeList(List<T> collection) {
+        return isNotEmpty(collection) ? collection : new ArrayList();
+    }
+
+    @SafeVarargs
+    public static <T> List<T> asList(T... list) {
+        if (list != null && list.length == 1 && list[0] == null) {
+            return new ArrayList();
+        } else {
+            List<T> result = new ArrayList();
+            if (isNotEmpty(list)) {
+                result.addAll(Arrays.asList(list));
+            }
+
+            return result;
+        }
+    }
 }
